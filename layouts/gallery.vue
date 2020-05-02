61656bb1fa264c53312199d57359cb1c84077e49
Arnold ZEPHIR <arnold.zephir@prevision.io>
2020-05-02 19:41:23 +0200

    feat: layout

--- layouts/gallery.vue
+++ layouts/gallery.vue
@@ -8,53 +8,21 @@
         <nuxt />
       </section>
     </main>
+    <footer>
+      <small>
+        Code source :
+        <a href="https://github.com/zarnold/infiniteFighting"
+          >https://github.com/zarnold/infiniteFighting</a
+        >
+      </small>
+    </footer>
   </div>
 </template>
 <style scoped>
-.container > * {
-  width: 30rem;
-  margin: 2rem auto;
-  padding: 1em;
-  box-sizing: border-box;
-}
+
 .container {
   display: flex;
-}
-
-header {
-  order: 1;
-}
-footer {
-  order: 5;
-}
-main {
-  background-color: hsl(144, 70%, 80%);
-  order: 3;
-}
-nav {
-  background-color: hsl(216, 70%, 80%);
-  order: 2;
-}
-aside {
-  background-color: hsl(288, 70%, 80%);
-  order: 4;
-}
-
-.container {
   flex-direction: row;
-  flex-wrap: wrap;
 }
-.container header,
-.container footer {
-  flex-basis: 100%;
-}
-.container main {
-  flex-grow: 1;
-}
-.container nav {
-  flex-basis: 25%;
-}
-.container aside {
-  flex-basis: 25%;
-}
+
 </style>
